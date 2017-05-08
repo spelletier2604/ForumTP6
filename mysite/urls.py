e2e091227a458bf89cbc6dedf28fd21d7bc2ca95
spelletier2604 <samuel.pelletier2014@outlook.com>
2017-05-08 19:55:28 -0400

    Details + CSS

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
