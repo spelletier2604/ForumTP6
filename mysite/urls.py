76313befab9ce34c9d89a5fdd436f59082313698
spelletier2604 <samuel.pelletier2014@outlook.com>
2017-05-08 21:30:13 -0400

    Revert "Butt load of stuff"
    This reverts commit fc8e43695614b49cfdf62b14b40c43265e902725.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^admin/', admin.site.urls),
     url(r'', include('forum.urls')),
 ]
